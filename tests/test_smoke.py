44142e316d1110f9933072371be7902246c5284e
WG-14 <pray5025@gmail.com>
2026-03-10 23:41:34 +0900

    Apply Codex patch 01

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,7 +1,13 @@
-﻿import py_compile
+import py_compile
+
 
 def test_main_compiles():
     py_compile.compile("main.py", doraise=True)
 
+
 def test_bot_compiles():
     py_compile.compile("bot.py", doraise=True)
+
+
+def test_package_main_compiles():
+    py_compile.compile("src/bithumb_bot/__main__.py", doraise=True)
